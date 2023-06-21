4f4fb01d1d1a2299891970d691fe71c6ab88ed1b
Rohit Kumar Sahu <91247476+rohit0796@users.noreply.github.com>
2023-06-21 22:42:20 +0530

    Update router.js

--- router.js
+++ router.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const schema = require('../server/Models/dbschema')
+const schema = require('./Models/dbschema')
 const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 const multer = require('multer')
