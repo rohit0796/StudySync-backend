eb8b31b76e81a734f2593987a54231e1f588679c
Rohit Kumar Sahu <91247476+rohit0796@users.noreply.github.com>
2023-07-16 11:59:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ mongoose.connect(datb, {
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+origin: "https://ivory-iguana-tutu.cyclic.app"
+}))
 app.use('/', urlRouter)
 const PORT= process.env.PORT|| 5000
 app.listen(PORT, () => {
